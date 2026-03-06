3a61f91e7924fec7daf3b9351adb079c8badd796
Stephen Davis <140756279+mod-io@users.noreply.github.com>
2026-03-05 21:31:54 -0500

    Implemented LRU algo

--- src/lru.py
+++ src/lru.py
@@ -1,1 +1,20 @@
+def lru(k, requests):
 
+    cache = []
+    misses = 0
+
+    for request in requests:
+
+        if request in cache:
+            cache.remove(request)
+            cache.append(request)
+
+        else:
+            misses += 1
+
+            if len(cache) == k:
+                cache.pop(0)
+
+            cache.append(request)
+        
+    return misses
--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
+import sys 
+import lru
 
+k = 3
+requests = [1,2,3,4,5,1,2,3,1,2,3,4]
+
+print("LRU  :", lru.lru(k, requests))
